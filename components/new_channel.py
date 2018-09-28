2957bb3efa0b233b2e00a43d223b50ca7f3edb79
Halk-lai Liff <werberth.lins@gmail.com>
2018-09-28 09:23:34 -0300

    Delete new_channel.py

--- components/new_channel.py
+++
@@ -1,8 +0,0 @@
-"""import datetime
-import time
-
-#t = datetime.datetime.utcnow()
-a = time.time()
-print(a)
-print(time.gmtime(a))
-print(datetime.datetime.fromtimestamp(time.mktime(datetime.datetime.utcnow().timetuple())).strftime("%b %d"))"""
